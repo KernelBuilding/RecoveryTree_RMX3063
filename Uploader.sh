f8bb570fdb249a50c8439ad2ad9d14966f6fe320
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-05-07 03:54:36 +0700

    Update Uploader.sh

--- Uploader.sh
+++ Uploader.sh
@@ -1,20 +1,38 @@
-#!/bin/bash
+*#!/bin/bash
 
 # Konfigurasi Telegram
 BOT_TOKEN="6868662734:AAE2WyTGytSFbfV0jjDnP_hmtXh4RMZ59Yw"
 CHAT_ID="-1002042015183"
 
 # Variabel pesan
-MESSAGE="*Ini pesan dengan foto dan tombol
-[Link Download](https://github.com/KernelBuilding/RecoveryTree_RMX3063)*."
+MESSAGE="
+SkyHawk Recovery Reborn
+
+Build Date : 06/05/2024
+Build By : @ProjectKernel2
+
+🔹 download
+🔹 screenshot
+
+Changelog :
+• Initial Recovery Build
+
+Bug :
+• Cannot Decrypt Data With Lockscreen Pass
+
+Spesial Thanks :
+• @Ipank_0p For Testing
+• @RanYakhsza 
+
+#shrp #unofficial #recovery #RMX3063
+"
 IMAGE_PATH="banner.png"
 BUTTON_TEXT="Download"
 REPLY_MARKUP='{"inline_keyboard": [[{"text": "'"$BUTTON_TEXT"'", "url": "https://github.com/KernelBuilding/RecoveryTree_RMX3063"}]]}'
 
 # KIRIM PESAN DENGAN FOTO DAN TOMBOL
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendPhoto" \
--d "chat_id=$CHAT_ID" \
--d "photo=@$IMAGE_PATH" \
--d "caption=$MESSAGE" \
--d "reply_markup=$REPLY_MARKUP"
--d "parse_mode=Markdown"
+-F chat_id="$CHAT_ID" \
+-F photo="@$IMAGE_PATH" \
+-F caption="$MESSAGE" \
+-F reply_markup="$REPLY_MARKUP"
