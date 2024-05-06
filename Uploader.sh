abd5607fadddaff1cc640cf5dafd1a3beadcb6ef
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-05-07 03:17:13 +0700

    Update Uploader.sh

--- Uploader.sh
+++ Uploader.sh
@@ -5,7 +5,8 @@ BOT_TOKEN="6868662734:AAE2WyTGytSFbfV0jjDnP_hmtXh4RMZ59Yw"
 CHAT_ID="-1002042015183"
 
 # Variabel pesan
-MESSAGE="Ini pesan dengan foto dan tombol."
+MESSAGE="Ini pesan dengan foto dan tombol
+[Link Download](https://github.com/KernelBuilding/RecoveryTree_RMX3063)."
 IMAGE_PATH="banner.png"
 BUTTON_TEXT="Download"
 REPLY_MARKUP='{"inline_keyboard": [[{"text": "'"$BUTTON_TEXT"'", "url": "https://github.com/KernelBuilding/RecoveryTree_RMX3063"}]]}'
