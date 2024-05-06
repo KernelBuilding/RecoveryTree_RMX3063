addb289683de66fdeacb017ad34cafec40bbef71
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-05-07 03:37:39 +0700

    Update Uploader.sh

--- Uploader.sh
+++ Uploader.sh
@@ -5,12 +5,13 @@ BOT_TOKEN="6868662734:AAE2WyTGytSFbfV0jjDnP_hmtXh4RMZ59Yw"
 CHAT_ID="-1002042015183"
 
 # Variabel pesan
-MESSAGE="SkyHawk Recovery Reborn
+MESSAGE="
+SkyHawk Recovery Reborn
 
 Build Date : 06/05/2024
 Build By : @ProjectKernel2
 
-🔹 download
+🔹 [download](https://github.com/KernelBuilding/RecoveryTree_RMX3063)
 🔹 screenshot
 
 Changelog :
@@ -23,7 +24,8 @@ Spesial Thanks :
 • @Ipank_0p For Testing
 • @RanYakhsza 
 
-#shrp #unofficial #recovery #RMX3063"
+#shrp #unofficial #recovery #RMX3063
+"
 IMAGE_PATH="banner.png"
 BUTTON_TEXT="Download"
 REPLY_MARKUP='{"inline_keyboard": [[{"text": "'"$BUTTON_TEXT"'", "url": "https://github.com/KernelBuilding/RecoveryTree_RMX3063"}]]}'
