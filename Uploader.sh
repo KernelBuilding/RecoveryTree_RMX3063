6370b5430d9497b61dc31fc7c46870575275596d
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-05-07 03:09:48 +0700

    Update Uploader.sh

--- Uploader.sh
+++ Uploader.sh
@@ -2,23 +2,17 @@
 
 # Konfigurasi Telegram
 BOT_TOKEN="6868662734:AAE2WyTGytSFbfV0jjDnP_hmtXh4RMZ59Yw"
-CHAT_ID="-1002042015183
+CHAT_ID="-1002042015183"
 
-# Isi pesan
-TEXT="Link : https://github.com/KernelBuilding/RecoveryTree_RMX3063"
+# Variabel pesan
+MESSAGE="Ini pesan dengan foto dan tombol."
+IMAGE_PATH="banner.png"
+BUTTON_TEXT="Download"
+REPLY_MARKUP='{"inline_keyboard": [{"text": "'"$BUTTON_TEXT"'", "url": "https://github.com/KernelBuilding/RecoveryTree_RMX3063"}]}'
 
-# Tombol
-BUTTON="download"
-
-# Markup
-MARKUP='{"inline_keyboard": [[{"text": "'"$BUTTON"'", "url": "https://github.com/KernelBuilding/RecoveryTree_RMX3063"}]], "resize_keyboard": true}'
-
-# Foto
-IMAGE="banner.png"
-
-# Kirim pesan
-curl -s \
-  -X POST \
-  -H "Content-Type: application/json" \
-  -d '{"chat_id":"'$CHAT_ID'", "text":"'$TEXT'", "reply_markup":'$MARKUP',"photo":"https://telegra.ph/file/bb7ac0ba4dea64f11318e.png"}' \
-  "https://api.telegram.org/bot$BOT_TOKEN/sendPhoto"
+# KIRIM PESAN DENGAN FOTO DAN TOMBOL
+curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendPhoto" \
+-F chat_id="$CHAT_ID" \
+-F photo="@$IMAGE_PATH" \
+-F caption="$MESSAGE" \
+-F reply_markup="$REPLY_MARKUP"
