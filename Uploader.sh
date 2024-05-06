fe57e884cedf23706b59fd12bcd41450402ae91a
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-05-07 03:22:47 +0700

    Update Uploader.sh

--- Uploader.sh
+++ Uploader.sh
@@ -5,15 +5,16 @@ BOT_TOKEN="6868662734:AAE2WyTGytSFbfV0jjDnP_hmtXh4RMZ59Yw"
 CHAT_ID="-1002042015183"
 
 # Variabel pesan
-MESSAGE="Ini pesan dengan foto dan tombol. - [Link Download](https://github.com/KernelBuilding/RecoveryTree_RMX3063)."
+MESSAGE="Ini pesan dengan foto dan tombol
+[Link Download](https://github.com/KernelBuilding/RecoveryTree_RMX3063)."
 IMAGE_PATH="banner.png"
 BUTTON_TEXT="Download"
-REPLY_MARKUP="{"inline_keyboard": [[{"text": "$BUTTON_TEXT", "url": "https://github.com/KernelBuilding/RecoveryTree_RMX3063"}]]}"
+REPLY_MARKUP='{"inline_keyboard": [[{"text": "'"$BUTTON_TEXT"'", "url": "https://github.com/KernelBuilding/RecoveryTree_RMX3063"}]]}'
 
 # KIRIM PESAN DENGAN FOTO DAN TOMBOL
-curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendPhoto" 
--F chat_id="$CHAT_ID" 
--F photo="@$IMAGE_PATH" 
--F caption="$MESSAGE" 
--F parse_mode="MarkdownV2" 
+curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendPhoto" \
+-F chat_id="$CHAT_ID" \
+-F photo="@$IMAGE_PATH" \
+-F caption="$MESSAGE" \
+-F parse_mode="MarkdownV2" \
 -F reply_markup="$REPLY_MARKUP"
