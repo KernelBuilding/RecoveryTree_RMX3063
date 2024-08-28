7fdc3301686935fc388364a9dd7d7ca9d73b019e
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-08-28 09:12:51 +0700

    RMX3063 : Remove  TW_SCREEN_BLANK_ON_BOOT := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,7 +144,6 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
 TW_NO_SCREEN_BLANK := true
-TW_SCREEN_BLANK_ON_BOOT := true
 TARGET_USES_MKE2FS := true
 TW_OZIP_DECRYPT_KEY := 0000
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone3/temp"
